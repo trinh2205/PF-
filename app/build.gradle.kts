174cc22826108f5776bf7ecd296d246e272e0124
trinh2205 <trinhntc.23ceb@vku.udn.vn>
2025-04-24 00:05:52 +0700

    Build tam du lieu cho bieu do

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,6 +106,11 @@ dependencies {
 //    }
     implementation("com.github.skydoves:landscapist-coil:2.4.7")
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+    implementation("com.patrykandpatrick.vico:compose:1.13.0")
+    implementation("com.patrykandpatrick.vico:core:1.13.0")
+    implementation("com.patrykandpatrick.vico:compose-m3:1.13.0")
+
 
     // Testing
     testImplementation(libs.junit)
